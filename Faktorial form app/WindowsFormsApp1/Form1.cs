fb9196d9f21c34f94d874f6eb202877515cab090
JavidSelimov12345 <44189780+JavidSelimov12345@users.noreply.github.com>
2019-12-30 12:45:30 +0400

    factorial added

---
+++ Faktorial form app/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Factorial : Form
+    {
+        public Factorial()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                long x = Convert.ToInt32(txtb1.Text);
+
+               long w = Fakt(x);
+               txtb2.Text = w.ToString();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Reqem daxil ele","Sehvvvvv",MessageBoxButtons.OK);
+            }
+            
+        }
+
+        static long Fakt(long i)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                i *= Fakt(i - 1);
+                return i;
+            }
+        }
+    }
+}
